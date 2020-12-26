3cc49a7acba32f36c843ef316fe06b07d87345de
ramesh <rsoft.ramesh@gmail.com>
2020-12-26 07:20:44 +0530

    feat: read collection and convert

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+const {safeDump} = require('js-yaml');
+const {readFile} = require('lint-staged');
+const {getInfo, getServers, getExternalDocs} = require('collectionInfo');
+
+const p2o = async (collectionPath, options = {}) => {
+  const collectionFile = await readFile(collectionPath);
+  const postmanJson = JSON.parse(collectionFile);
+  const openAPI = {
+    ...getInfo(postmanJson),
+    ...getExternalDocs(postmanJson),
+    ...getServers(postmanJson),
+    ...options,
+    openapi: '3.0.3',
+  };
+  return safeDump(openAPI, {skipInvalid: true});
+};
+
+module.exports = p2o;
+
