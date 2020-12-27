cabc13896fbc0440927a88a5ff08298ea862ac90
ramesh <rsoft.ramesh@gmail.com>
2020-12-27 15:39:25 +0530

    feat: basic auth

--- src/extractors/security.js
+++ src/extractors/security.js
@@ -1,4 +1,18 @@
-const getBasicAuth = ({basic}, schema) => {
+const {findVariable} = require('./collectionInfo');
+const {clean} = require('../util/validation');
+
+const getAuth = ({auth, variable}) => {
+  const description = findVariable(variable, 'authDescription');
+  if (auth?.type === 'basic') {
+    return {
+      basicAuth: clean({
+        type: 'http',
+        scheme: 'basic',
+        description,
+      }),
+    };
+  }
+  return {};
 };
 
-exports.getBasicAuth = getBasicAuth;
+exports.getAuth = getAuth;
--- src/extractors/security.test.js
+++ src/extractors/security.test.js
@@ -1,16 +1,13 @@
 const {
-  getBasicAuth,
+  getAuth,
 } = require('./security');
-const {SUPPORTED} = require('../index');
 const {expect} = require('chai');
 
 describe('Test collection security parser', () => {
-  describe('Test getBasicAuth', () => {
-    SUPPORTED.forEach(({schema}) => {
-      it('Test with null data', () => {
-        const actual = getBasicAuth({basic: {}}, schema);
-        expect(actual).deep.equal({});
-      });
+  describe('Test getAuth', () => {
+    it('Test with null data', () => {
+      const actual = getAuth({});
+      expect(actual).deep.equal({});
     });
   });
 });
