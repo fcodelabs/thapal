bf6caed4e5358a00332c1c4139e336d976e42f4a
ramesh <rsoft.ramesh@gmail.com>
2020-12-27 16:13:53 +0530

    feat: api key auth

--- src/extractors/security.js
+++ src/extractors/security.js
@@ -1,16 +1,40 @@
 const {findVariable} = require('./collectionInfo');
 const {clean} = require('../util/validation');
 
-const getAuth = ({auth, variable}) => {
+const getBasicAuth = (description) => ({
+  BasicAuth: clean({
+    type: 'http',
+    scheme: 'basic',
+    description,
+  }),
+});
+
+const getApiKeyAuth = (authData, description, schema) => {
+  let _in = undefined;
+  let name = undefined;
+  if (schema === '2.0.0') {
+    _in = authData['in'];
+    name = authData['key'];
+  } else if (schema === '2.1.0') {
+    _in = findVariable(authData, 'in');
+    name = findVariable(authData, 'key');
+  }
+  const auth = {
+    type: 'apiKey',
+    in: _in,
+    name,
+    description,
+  };
+  return {ApiKeyAuth: clean(auth)};
+};
+
+const getAuth = ({auth, variable}, schema) => {
   const description = findVariable(variable, 'authDescription');
   if (auth?.type === 'basic') {
-    return {
-      basicAuth: clean({
-        type: 'http',
-        scheme: 'basic',
-        description,
-      }),
-    };
+    return getBasicAuth(description);
+  } else if (auth?.type === 'apikey') {
+    const authData = auth['apikey'];
+    return getApiKeyAuth(authData, description, schema);
   }
   return {};
 };
--- src/extractors/security.test.js
+++ src/extractors/security.test.js
@@ -4,10 +4,49 @@ const {
 const {expect} = require('chai');
 
 describe('Test collection security parser', () => {
-  describe('Test getAuth', () => {
-    it('Test with null data', () => {
-      const actual = getAuth({});
-      expect(actual).deep.equal({});
+  it('Test with null data', () => {
+    expect(getAuth({})).deep.equal({});
+    expect(getAuth({}, '1234')).deep.equal({});
+  });
+
+  const variable = [
+    {
+      key: 'authDescription',
+      value: 'This is the basic auth description',
+    },
+  ];
+
+  describe('Check basic authentication', () => {
+    const basicAuth = {
+      type: 'basic',
+      basic: [],
+    };
+    const expectedBasic = {
+      BasicAuth: {
+        type: 'http',
+        scheme: 'basic',
+      },
+    };
+
+    it('Check basic auth without description', () => {
+      const actual = getAuth({auth: basicAuth}, '1234');
+      expect(actual).deep.equal(expectedBasic);
     });
+
+    it('Check basic auth with description', () => {
+      const input = {
+        auth: basicAuth,
+        variable,
+      };
+      const expected = {
+        ...expectedBasic,
+      };
+      expected.BasicAuth['description'] = 'This is the basic auth description';
+      const actual = getAuth(input, '1234');
+      expect(actual).deep.equal(expected);
+    });
+  });
+
+  describe('Check API key authentication', () => {
   });
 });
